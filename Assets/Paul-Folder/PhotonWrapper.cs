a82b5184ed03a09c54aa19ebee448b8d02e5341e
Paul Boutot <paulvboutot@hotmail.com>
2019-09-05 19:47:16 -0400

    added code

---
+++ Assets/Paul-Folder/PhotonWrapper.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class PhotonWrapper : MonoBehaviourPunCallbacks
+{
+    public void Connect() {
+        PhotonNetwork.ConnectUsingSettings();
+         
+        // TODO: move out of here
+
+    }
+
+    public void LoadScene() {
+        PhotonNetwork.LoadLevel("SampleScene");
+    }
+
+    public override void OnConnectedToMaster() {
+        Debug.Log("this was called.");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+        
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    public override void OnJoinedRoom() {
+        LoadScene();
+    }
+}
