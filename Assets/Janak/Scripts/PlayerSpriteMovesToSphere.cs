68fadd55ec859956d924635a09b9eacdd1f1948f
Janakkumar Bhanushali <jbhanush@ws.cs.ryerson.ca>
2019-09-19 19:53:57 -0400

    note to self: was working on adding more key frames to player_walking_left

---
+++ Assets/Janak/Scripts/PlayerSpriteMovesToSphere.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpriteMovesToSphere : MonoBehaviour
+{
+    private Transform my_t;
+
+    public GameObject player_sphere;
+    private Transform sphere_t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        my_t = GetComponent<Transform>();
+
+        sphere_t = player_sphere.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        my_t.position = sphere_t.position;
+    }
+}
